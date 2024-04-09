1e7bee3b8a4ef4ed3a824521a7609e9c67a20e06
code-with-raviyadav <166327030+code-with-raviyadav@users.noreply.github.com>
2024-04-09 23:03:24 +0530

    Add files via upload

---
+++ ecommerce.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Apna Store</title>
+<link rel="stylesheet" href="red.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+<div class="header">
+
+<div class="cointainer">
+
+<div class="navbar">
+<div class="logo">
+<img src="images/slider/red.png"
+
+</div>
+<nav>
+<ul>
+<li><a href="">Home</a></li>
+<li><a href="">Products</a></li>
+<li><a href="">About</a></li>
+<li><a href="">Contact</a></li>
+<li><a href="">Account</a></li>
+</ul>
+</nav>
+<img src="images/slider/cart.png" height="30px" width="30px">
+
+</div>
+
+<div class="row">
+
+<div class="col-2">
+<h1> Give Your Workout<br>  A New Style</h1>
+<p> Success Isn't Always about Greatness. It's about consistent hard work gains success. Greatness will come.</p>
+<a href="" class="btn"> Explore Now &#8594;</a>
+
+</div>
+<div class="col-2">
+<img src="images/slider/image1.png" >
+</div>
+</div>
+</div>
+</div>
+<!------featured categories------>
+<div class="categories">
+<div class="small-container">
+
+<div class="row">
+<div class="col-3">
+
+<img src="images/slider/Banner2.jpg" width="100px" height="300px" >
+</div>
+<div class="col-3">
+
+<img src="images/slider/Banner6 (2).jpg" width="100px" height="300px" ></div>
+<div class="col-3">
+
+
+<img src="images/slider/box4.jpg" width="100px" height="300px"></div>
+</div>
+</div>
+</div>
+<!-------featured product------->
+<div class="small-container">
+<h2> Feature Product</h2>
+<div class="row">
+
+<div class="col-4">
+<img src="images/slider/Banner1.jpg" width="300px" height="300px">
+<h4>Red Printed Tshirt</h4>
+<div class="rating">
+</div>
+<p>$50.00</p>
+</div>
+
+<div class="col-4">
+<img src="images/slider/Banner1.jpg" width="300px" height="300px">
+<h4>Red Printed Tshirt</h4>
+<div class="rating">
+</div>
+<p>$50.00</p>
+
+</div>
+<div class="col-4">
+<img src="images/slider/Banner1.jpg" width="300px" height="300px">
+<h4>Red Printed Tshirt</h4>
+<div class="rating">
+<i class="fa fa-star"></li>
+</div>
+<p>$50.00</p>
+
+</div>
+<div class="col-4">
+<img src="images/slider/Banner1.jpg" width="300px" height="300px">
+<h4>Red Printed Tshirt</h4>
+<div class="rating">
+</div>
+<p>$50.00</p>
+
+</div>
+</div>
+</div>
+
+<!----Offer--->
+<div class="offer">
+<div class="small-container">
+<div class="row">
+<div class=" col-2">
+<img src="images/slider/english (1).jpg" class="offer-img">
+</div>
+
+<div class="col-2">
+<p>Exclusively Available on Redstore</p>
+<h1>Smart band</h1>
+<small> the MI Smart band 4 features a 39.9% larger
+ then Mi brand 3) amoled color full touch display with
+  adjustable brightness ,so everything is clear as can 
+  be.</small>
+</div>
+</div>
+</div>
+</div>
+</body>
+
+</html>
+
